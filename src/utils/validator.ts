7c021c0db8391aaa05575c419c833c2033be8682
Ekaterina <ches.ekaterina2010@yandex.ru>
2022-09-14 22:29:41 +0300

    fix validation error

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -37,6 +37,7 @@ export function validate(nameInput: string, valueInput: string) {
     setErrorMes(nameInput, validationRules[nameInput].error);
   } else {
     removeError(nameInput);
+    removeError('form')
   }
 }
 
